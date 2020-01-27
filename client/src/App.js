00ae23c1b679f7cefd90aea8dfeeb4b776bc3f83
Helena Gourrand <helena.gourrand@gmail.com>
2020-01-27 14:50:53 +0100

    [WIP] Redux + rebuild + get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 import {RadialBarChart, RadialBar, Legend, Tooltip, PieChart, Pie} from 'recharts';
@@ -9,67 +9,65 @@ import CardContent from "@material-ui/core/CardContent";
 import PieComponent from "./components/PieComponent";
 import {Container} from "@material-ui/core";
 import TableComponent from "./components/utils/TableComponent";
+import axios from "axios";
 
 
 function App() {
 
-	const data = [
-		{
-			"name": "iron",
-			"uv": 30,
-			"fill": "#ccc"
-		},
-		{
-			"name": "redstone",
-			"uv": 15,
-			"fill": "#f00"
-		},
-		{
-			"name": "gold",
-			"uv": 4,
-			"fill": "#ff0"
-		},
-		{
-			"name": "diamond",
-			"uv": 5,
-			"fill": "#0ff"
-		},
-		{
-			"name": "coal",
-			"uv": 100,
-			"fill": "#000"
-		},
-		{
-			"name": "air",
-			"uv": 1000,
-			"fill": "#fff"
-		},
-		{
-			"name": "stone",
-			"uv": 4000,
-			"fill": "#aaa"
-		}
-	];
 	const data02 = [
-		{name: "iron", value: 100, color: "#ccc", prout:10},
-		{name: "redstone", value: 300, color: "#C00", prout:10},
-		{name: "gold", value: 100, color: "#CC0", prout:10},
-		{name: "diamond", value: 80, color: "#00C", prout:11},
-		{name: "coal", value: 40, color: "#000", prout:11},
+		{name: "iron", value: 100, color: "#ccc", prout: 10},
+		{name: "redstone", value: 300, color: "#C00", prout: 10},
+		{name: "gold", value: 100, color: "#CC0", prout: 10},
+		{name: "diamond", value: 80, color: "#00C", prout: 11},
+		{name: "coal", value: 40, color: "#000", prout: 11},
 		{name: "air", value: 30, color: "#FFF"},
 		{name: "stone", value: 50, color: "#CCC"},
 	];
+	const axiosConfig = {
+		headers: {'Access-Control-Allow-Origin': '*'}
+	};
+	const [params, setParams] = useState({
+			data: {
+				chunks: [
+					{
+						x: 2,
+						z: 0
+					}
+				],
+				filter: {},
+				dim: "overworld",
+			}
+		});
+	const [data, refreshData] = useState(axios.get('http://localhost:8190/listeTypeBlocks?', {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Accept': 'application/json'
+			},
+			params: params,
+		})
+			.then(response => {
+				console.log(params);
+				console.log(data);
+				return (response.data);
+			})
+			.catch(function (error) {
+				console.log(params);
+				console.log(error);
+			}));
+
 	return (
 		<div className="App-content">
 			<div className="Container">
 				<Card className="Card-content">
 					<CardContent className="Card">
 						<div>
-							<PieComponent radius={150} thickness={20} data={data02} borderSize={10} borderColor={"black"} />
-							<TableComponent data={data02} />
+							<PieComponent radius={150} thickness={20} data={data02} borderSize={10}
+										  borderColor={"black"}/>
+							<TableComponent data={data02}/>
 						</div>
 					</CardContent>
 				</Card>
+					<button onClick={() => {axios.get('http://localhost:8190/debug/rebuild?', {params : {data:{radius : 2}}}).then(r => console.log(r)).catch(e => console.log(e))}}/>
 			</div>
 		</div>
 
